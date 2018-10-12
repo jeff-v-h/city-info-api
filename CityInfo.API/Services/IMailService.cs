870bb67b7e6df7bbf145bd95a2109641e24725aa
DELTATRE\Jeff.Huang <jeff.huang@deltatre.it>
2018-10-12 17:06:56 +0100

    add comment explaining use of mailservice

--- CityInfo.API/Services/IMailService.cs
+++ CityInfo.API/Services/IMailService.cs
@@ -5,6 +5,10 @@ using System.Threading.Tasks;
 
 namespace CityInfo.API.Services
 {
+    // The mail service is used in this project to notify by email when a point of interest
+    // has been deleted. Deleting something has quite an impact on consumers of APIs
+    // The SQL Server Database Engine can send e-mail messages to users.
+    // The database app uses Database Mail which uses standard Simple Mail Transfer Protocol (SMTP)
     public interface IMailService
     {
         void Send(string subject, string message);
